c5b167c0a4a53a0688402722cb1726e9caddc467
Matheus Rocco <62848343+msrocco@users.noreply.github.com>
2023-08-30 13:38:54 -0400

    feat: content calendar api

--- src/main/java/com/msrocco/contentcalendar/Application.java
+++ src/main/java/com/msrocco/contentcalendar/Application.java
@@ -1,17 +1,9 @@
 package com.msrocco.contentcalendar;
 
 import com.msrocco.contentcalendar.config.ContentCalendarProperties;
-import com.msrocco.contentcalendar.model.Content;
-import com.msrocco.contentcalendar.model.Status;
-import com.msrocco.contentcalendar.model.Type;
-import com.msrocco.contentcalendar.repository.ContentRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDateTime;
 
 @EnableConfigurationProperties(ContentCalendarProperties.class)
 @SpringBootApplication
